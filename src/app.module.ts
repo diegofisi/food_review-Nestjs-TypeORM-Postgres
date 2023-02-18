6aefcd718fb6077d70d75943382935cf66df7b48
diego <diego704249@gmail.com>
2023-02-18 12:13:39 -0500

    docker-compose add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, RolesModule, AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
