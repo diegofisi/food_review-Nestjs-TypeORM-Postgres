8cba542f53cefd75e0df013991737b9ddebd6030
diego <diego704249@gmail.com>
2023-03-04 15:37:20 -0500

    clearing modules

--- src/opinions/opinions.controller.ts
+++ src/opinions/opinions.controller.ts
@@ -6,11 +6,13 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { OpinionService } from './opinions.service';
 import { CreateOpinionDto } from './dto/create-opinion.dto';
 import { UpdateOpinionDto } from './dto/update-opinion.dto';
 import { Auth, GetUser } from 'src/auth/decorators';
+import { User } from 'src/auth/users/entities/user.entity';
 
 @Controller(':id/opinions')
 export class OpinionController {
@@ -18,22 +20,29 @@ export class OpinionController {
 
   @Post(':id')
   @Auth()
-  create(@Body() createOpinionDto: CreateOpinionDto, @GetUser() user) {
-    return this.opinionService.create(createOpinionDto);
+  create(@Body() createOpinionDto: CreateOpinionDto, @GetUser() user: User) {
+    return this.opinionService.create(createOpinionDto, user);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.opinionService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.opinionService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOpinionDto: UpdateOpinionDto) {
-    return this.opinionService.update(+id, updateOpinionDto);
+  @Patch(':id/opinions/:idOpinion')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Param('idOpinion', ParseUUIDPipe) idOpinion: string,
+    @Body() updateOpinionDto: UpdateOpinionDto,
+  ) {
+    return this.opinionService.update(id, idOpinion, updateOpinionDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.opinionService.remove(+id);
+  @Delete(':id/opinions/:idOpinion')
+  remove(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Param('idOpinion', ParseUUIDPipe) idOpinion: string,
+  ) {
+    return this.opinionService.remove(id, idOpinion);
   }
 }
--- src/opinions/opinions.service.ts
+++ src/opinions/opinions.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/auth/users/entities/user.entity';
 import { CreateOpinionDto } from './dto/create-opinion.dto';
 import { UpdateOpinionDto } from './dto/update-opinion.dto';
 
 @Injectable()
 export class OpinionService {
-  create(createOpinionDto: CreateOpinionDto) {
+  create(createOpinionDto: CreateOpinionDto, user: User) {
     return 'This action adds a new comment';
   }
 
@@ -12,15 +13,15 @@ export class OpinionService {
     return `This action returns all comments`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} comment`;
   }
 
-  update(id: number, updateOpinionDto: UpdateOpinionDto) {
+  update(id: string, idOpinion: string, updateOpinionDto: UpdateOpinionDto) {
     return `This action updates a #${id} comment`;
   }
 
-  remove(id: number) {
+  remove(id: string, idOpinion: string) {
     return `This action removes a #${id} comment`;
   }
 }
